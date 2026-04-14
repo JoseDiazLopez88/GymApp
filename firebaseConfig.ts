97e9376e877e04cc09f5f63c816ba9fd149a6ba8
Jose DL <jose.diaz.lopez94@gmail.com>
2026-04-14 08:27:30 -0500

    chore: secure sensitives credentials by moving to environment variables and updating gitignore

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore, doc, setDoc, getDoc, collection, query, where, getDocs, serverTimestamp } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDu-zVL2OJmn7Sys8nqpGt-2_CsPnOfQFg",
-  authDomain: "calmacoffeeapp.firebaseapp.com",
-  projectId: "calmacoffeeapp",
-  storageBucket: "calmacoffeeapp.firebasestorage.app",
-  messagingSenderId: "241052728524",
-  appId: "1:241052728524:web:c35d933555557a35e45669"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
